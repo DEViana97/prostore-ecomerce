c83672a08d71cf62b51220f7771375a1f328c8f7
deviana97 <mateusviana97@gmail.com>
2025-06-08 13:47:30 -0300

    fix imports

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Button } from "@/components/ui/button";
-import { DropdownMenuCheckboxItem } from "@/components/ui/dropdown-menu";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuCheckboxItem, DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuContent } from "@/components/ui/dropdown-menu";
 import { SunIcon, MoonIcon, SunMoon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
