771e687bc4bfaa0973fbd4bb0e116e074140f59f
deviana97 <mateusviana97@gmail.com>
2025-06-19 09:05:30 -0300

    feat: update Homepage to display product list with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
+import sampleData from "@/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const Homepage = () => {
   return (
-    <>prostore</>
+    <>
+      <ProductList
+        data={sampleData.products}
+        limit={4}
+        title="Newest Arrivals" />
+    </>
   );
 }
 
